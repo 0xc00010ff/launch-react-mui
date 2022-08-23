377cfb2519250d2bb5a6f6a28f2b4f0a862357da
Brian Cox <brian@therealbriancox.com>
2022-08-22 19:11:26 -0700

    Adds basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 
 export default function FilesExample() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
